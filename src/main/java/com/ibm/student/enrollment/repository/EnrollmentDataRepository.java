15848f9e0f4a1b6b38b2b9976422ed60c3d5c796
AmodKumarSingh <AmodKumarSingh@host.docker.internal>
2020-03-12 01:36:58 +0530

    Add input stream for reading file inside jar

--- src/main/java/com/ibm/student/enrollment/repository/EnrollmentDataRepository.java
+++ src/main/java/com/ibm/student/enrollment/repository/EnrollmentDataRepository.java
@@ -1,14 +1,13 @@
 package com.ibm.student.enrollment.repository;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 import javax.annotation.PostConstruct;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Repository;
 import org.springframework.util.CollectionUtils;
 
@@ -18,30 +17,23 @@ import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ibm.student.enrollment.entity.Enrollment;
 
-import lombok.AccessLevel;
 import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
 
 @Data
 @Repository
 public class EnrollmentDataRepository {
-
-	@Getter(AccessLevel.NONE)
-	@Setter(AccessLevel.NONE)
-	@Value("classpath:output.json")
-	private Resource dataFile;
 
 	private List<Enrollment> enrollments;
 
 	@PostConstruct
 	public void init() throws JsonParseException, JsonMappingException, IOException {
 		ObjectMapper mapper = new ObjectMapper();
-		enrollments = Collections
-				.unmodifiableList(mapper.readValue(dataFile.getFile(), new TypeReference<List<Enrollment>>() {
-				}));
-		if(CollectionUtils.isEmpty(enrollments)) {
-			enrollments = new ArrayList<>();
+		try (InputStream is = this.getClass().getClassLoader().getResourceAsStream("output.json")) {
+			enrollments = Collections.unmodifiableList(mapper.readValue(is, new TypeReference<List<Enrollment>>() {
+			}));
+			if (CollectionUtils.isEmpty(enrollments)) {
+				enrollments = new ArrayList<>();
+			}
 		}
 	}
 }
